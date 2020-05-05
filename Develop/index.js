e07d1ce00511561277dfbe3471d186b4dfd3f7e9
taypal <tay.houk@yahoo.com>
2020-05-04 19:30:10 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,15 @@
 const questions = [
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username"
+
+    },
+    {
+        type: "input",
+        message: "What is your favorite color?",
+        name: "color"
+    }
 
 ];
 
